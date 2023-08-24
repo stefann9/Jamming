b04ed74b60393e853425090710bb5b0ae3d178e9
stefann9 <105439203+stefann9@users.noreply.github.com>
2023-08-24 14:51:22 +0300

    add renderAction in Track comp

--- src/components/Track/Track.jsx
+++ src/components/Track/Track.jsx
@@ -1,13 +1,18 @@
 import "./Track.css";
 
-const Track = () => {
+const Track = (isRemoval ) => {
+
+  const renderAction = () => {
+    return <button className="Track-action">{isRemoval ? "-" : "+"}</button>;
+  };
+  
   return (
     <div className="Track">
       <div className="Track-information">
         <h3>track name will go here </h3>
         <p> track artist will go here | track album will go here</p>
       </div>
-      <button className="Track-action"> + or - will go here </button>
+      {renderAction()}
     </div>
   );
 };
