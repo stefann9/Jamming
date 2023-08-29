4339d39fac1f1d826fb5e9be2683e0196e226fa2
stefann9 <105439203+stefann9@users.noreply.github.com>
2023-08-29 13:07:03 +0300

    add CurrentPlaylist comp onyl static

---
+++ src/components/CurrentPlaylist/CurrentPlaylist.jsx
@@ -0,0 +1,11 @@
+import "./CurrentPlaylist.css";
+const CurrentPlaylist = () => {
+  return (
+    <div className="User-Playlist">
+      <h3>Name</h3>
+      <button className="Track-action">+</button>
+    </div>
+  );
+};
+
+export default CurrentPlaylist;
--- src/components/UserPlaylists/UserPlaylists.jsx
+++ src/components/UserPlaylists/UserPlaylists.jsx
@@ -1,9 +1,11 @@
+import CurrentPlaylist from "../CurrentPlaylist/CurrentPlaylist";
 import "./UserPlaylists.css";
 
 const UserPlaylists = () => {
   return (
     <div className="UserPlaylists">
-      <h1>Current playlists</h1>
+      <h1>Your playlists</h1>
+      <CurrentPlaylist />
       <button className="Playlist-select">New Playlist</button>
     </div>
   );
